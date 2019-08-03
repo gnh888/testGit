d369c0711ed6d1203bab1481cea751491ab86a3e
username <username@example.com>
2019-08-03 14:49:36 +0800

    甲提交一次

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("开始提交");
-        System.out.println("提交一次");
+        System.out.println("甲提交一次");
     }
 }
